4c031216dc8a48c41e21413354b300a90c0385a4
StrappedGlint13 <matiasbrax@gmail.com>
2020-06-22 17:09:34 +0300

    4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
   }
@@ -8,8 +10,31 @@ const totalLikes = (blogs) => {
 
     return totalLikes
 }
+
+const favoriteBlog = (blogs) => {
+    if (typeof blogs === 'undefined' || blogs.length === 0) {
+         return 'The array is not defined or is empty'
+    }
+    
+    if (blogs.length === 1) {
+        return blogs[0]
+    } else {
+        let most_likes = {'likes':-1}
+
+        blogs.forEach(blog => 
+            {
+                if(blog.likes > most_likes.likes) {
+                    most_likes = blog
+                } 
+            })
+
+        return most_likes
+    }
+
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
