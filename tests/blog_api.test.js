ea1687e24885e6af58aeb62dc3d1195cf863f97b
StrappedGlint13 <matiasbrax@gmail.com>
2020-06-24 19:56:43 +0300

    4.9 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [ 
+    { title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 }, 
+    { title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 }, 
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+  
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+  
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
 
 describe('GET tests', () => {
     test('blogs are returned as json', async () => {
@@ -12,13 +27,21 @@ describe('GET tests', () => {
         .expect('Content-Type', /application\/json/)
     })
 
-    test('there are three blogs', async () => {
+    test('there are two blogs', async () => {
         const response = await api.get('/api/blogs')
     
-        expect(response.body).toHaveLength(3)
+        expect(response.body).toHaveLength(initialBlogs.length)
     })
+
+    test('id is defined right', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].id).toBeDefined()
+      })
 
     afterAll(() => {
     mongoose.connection.close()
     })
 })
+
+
